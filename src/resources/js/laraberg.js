d0d6e099a47ef4721fa3982bb3c0cb3de5d418fe
Maurice Wijnia <mauricewijnia@live.nl>
2019-02-28 13:26:50 +0100

    Remove normalize.css and move all css imports to laraberg.scss

--- src/resources/js/laraberg.js
+++ src/resources/js/laraberg.js
@@ -5,12 +5,6 @@ import 'babel-polyfill'
 import './gutenberg/settings'
 import initGutenberg from './gutenberg/init-gutenberg'
 
-import 'normalize.css'
-
-// Gutenberg JS Style
-import '@frontkom/gutenberg-js/build/css/block-library/style.css'
-import '@frontkom/gutenberg-js/build/css/style.css'
-
 import './../scss/laraberg.scss'
 
 window.Laraberg = {
