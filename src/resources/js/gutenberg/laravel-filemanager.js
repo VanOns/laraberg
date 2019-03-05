408de7e8bcc0cc2ffef2a12ce57ca37f4452dc58
Maurice Wijnia <mauricewijnia@live.nl>
2019-03-05 16:33:44 +0100

    Let filemanager automatically paste url in 'Insert from URL' field and submit the field

--- src/resources/js/gutenberg/laravel-filemanager.js
+++ src/resources/js/gutenberg/laravel-filemanager.js
@@ -3,12 +3,12 @@ import elementAppears from './element-appears'
 
 export default async function setupLaravelFilemanager () {
   elementAppears('.editor-media-placeholder', mediaEditor => {
-    let lfmButton = document.createElement('button')
+    const lfmButton = document.createElement('button')
     lfmButton.innerHTML = 'File Manager'
     lfmButton.classList.add('components-button', 'editor-media-placeholder__button', 'is-button', 'is-default', 'is-large')
     lfmButton.type = 'button'
     lfmButton.addEventListener('click', lfmListener)
-    let fileUpload = mediaEditor.querySelector('.components-form-file-upload')
+    const fileUpload = mediaEditor.querySelector('.components-form-file-upload')
     fileUpload.parentNode.insertBefore(lfmButton, fileUpload.nextSibling)
   })
 }
@@ -24,7 +24,7 @@ function fireEvent (el, etype) {
 }
 
 function lfmListener (event) {
-  let block = event.target.parentNode.parentNode.parentNode
+  const block = event.target.parentNode.parentNode.parentNode
   console.log(block)
   let type
   if (block.querySelector('.wp-block-image') !== null) {
@@ -44,13 +44,15 @@ function lfm (options, cb) {
 }
 
 async function insertImage (element, url) {
-  let urlButton = element.querySelector('.editor-media-placeholder__url-input-container').querySelector('button')
-  fireEvent(urlButton, 'click')
-  let formSelector = '.editor-media-placeholder__url-input-form'
+  const urlButton = element.querySelector('.editor-media-placeholder__url-input-container').querySelector('button')
+  urlButton.click()
+  const formSelector = '.editor-media-placeholder__url-input-form'
   await elementReady(formSelector)
-  let urlForm = document.querySelector(formSelector)
-  let urlInput = urlForm.querySelector('input')
-  urlInput.setAttribute('value', url)
+  const urlForm = document.querySelector(formSelector)
+  const urlInput = urlForm.querySelector('input')
+  urlInput.value = url
+  urlInput.value += ' '
   fireEvent(urlInput, 'change')
-  // TODO: Click submit button
+  const submitButton = urlForm.querySelector('button')
+  submitButton.click()
 }
