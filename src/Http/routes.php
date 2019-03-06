2bb571b6f53bda9a9b48fd80fbcaeb75193a013d
Maurice Wijnia <mauricewijnia@live.nl>
2019-03-06 12:05:27 +0100

    Remove media API endpoints

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,8 +2,6 @@
 
 $namespace = 'MauriceWijnia\Laraberg\Http\Controllers\\';
 Route::apiResource('laraberg/blocks', $namespace.'BlockController');
-Route::apiResource('laraberg/media', $namespace.'MediaController');
-
 Route::get('laraberg/oembed', $namespace.'OEmbedController');
 
 Route::get('/laravel-filemanager', '\UniSharp\LaravelFilemanager\Controllers\LfmController@show');
