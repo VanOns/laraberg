232f195c166e915d4378227233b24a9fe6c5868a
Maurice Wijnia <mauricewijnia@live.nl>
2019-03-12 16:43:10 +0100

    Remove laravel-filemanager from the package

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,6 +3,3 @@
 $namespace = 'MauriceWijnia\Laraberg\Http\Controllers\\';
 Route::apiResource('laraberg/blocks', $namespace.'BlockController');
 Route::get('laraberg/oembed', $namespace.'OEmbedController');
-
-Route::get('/laravel-filemanager', '\UniSharp\LaravelFilemanager\Controllers\LfmController@show');
-Route::post('/laravel-filemanager/upload', '\UniSharp\LaravelFilemanager\Controllers\UploadController@upload');
