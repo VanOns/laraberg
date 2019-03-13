42dadf28c909c246ca20c3149b6275d59799463b
Maurice Wijnia <mauricewijnia@live.nl>
2019-03-13 12:10:19 +0100

    Add public in front of functions in Content model

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -17,14 +17,14 @@ class Content extends Model {
   /**
    * Returns the rendered content of the content
    */
-  function render() {
+  public function render() {
     $html = $this->rendered_content;
     $html = $this->renderBlocks($html);
     $html = "<div class='gutenberg__content wp-embed-responsive'>$html</div>";
     return $html;
   }
 
-  function setContent($html) {
+  public function setContent($html) {
     $this->raw_content = $html;
     $this->rendered_content = $this->renderRaw($html);
   }
@@ -32,14 +32,14 @@ class Content extends Model {
   /**
    * Renders the HTML of the content object
    */
-  function renderRaw($raw) {
+  public function renderRaw($raw) {
     $html = $raw;
     $html = $this->renderEmbeds($html);
     event(new ContentRendered($this));
     return $html;
   }
 
-  function renderBlocks($html) {
+  public function renderBlocks($html) {
     $regex = '/<!-- wp:block {"ref":(\d*)} \/-->/';
     $result = preg_replace_callback($regex, function($matches) {
       try {
@@ -51,7 +51,7 @@ class Content extends Model {
     return $result;
   }
 
-  function renderBlock($matches) {
+  public function renderBlock($matches) {
     if ($matches) {
       $content = Block::find($matches[1])->content['raw'];
       $content = $this->renderBlocks($content);
@@ -59,7 +59,7 @@ class Content extends Model {
     }
   }
 
-  function renderEmbeds($html) {
+  public function renderEmbeds($html) {
     $result = preg_replace_callback('/<!-- wp:core-embed\/.*?--><figure class="wp-block-embed.*?".*?<div class="wp-block-embed__wrapper">(.*?)<\/div><\/figure>/', function($matches) {
       $embed = Embed::create($matches[1])->code;
       return str_replace('>'.$matches[1].'<', '>'.$embed.'<', $matches[0]);
