326fde6d18ae6ec345a2467825740fcc4968539a
Maurice Wijnia <mauricewijnia@live.nl>
2019-03-20 14:06:44 +0100

    Change inline documentation indentation

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -19,9 +19,9 @@ class Content extends Model
     }
 
     /**
-   * Returns the rendered content of the content
-   * @return String - The completely rendered content
-   */
+     * Returns the rendered content of the content
+     * @return String - The completely rendered content
+     */
     public function render()
     {
         $html = BlockHelper::renderBlocks($this->rendered_content);
@@ -29,9 +29,9 @@ class Content extends Model
     }
 
     /**
-   * Sets the raw content and performs some initial rendering
-   * @param String $html
-   */
+     * Sets the raw content and performs some initial rendering
+     * @param String $html
+     */
     public function setContent($html)
     {
         $this->raw_content = $html;
@@ -39,8 +39,8 @@ class Content extends Model
     }
 
     /**
-   * Renders the HTML of the content object
-   */
+     * Renders the HTML of the content object
+     */
     public function renderRaw()
     {
         $this->rendered_content = EmbedHelper::renderEmbeds($this->raw_content);
