c2cb5f7c270e1cf81cd65cac640cc1e307e862d0
Maurice Wijnia <mauricewijnia@live.nl>
2019-05-22 11:44:28 +0200

    [Sidebar] Refactor and fix sidebar radio button functionality

--- src/resources/js/sidebar/sidebar.js
+++ src/resources/js/sidebar/sidebar.js
@@ -19,48 +19,74 @@ export default function () {
 class Sidebar extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      elements: []
-    }
+
+    this.inputs = []
+    this.radioCache = []
+    this.state = { elements: [] }
+
+    this.mustFlushRadio = this.mustFlushRadio.bind(this)
+    this.handleElement = this.handleElement.bind(this)
   }
 
   componentDidMount () {
     this.getElements()
   }
 
+  /**
+   * Get all sidebar elements and add them to the sidebar
+   */
   getElements () {
     const elements = Array.from(document.querySelectorAll(`.laraberg-sidebar *`))
-    let inputs = []
-    let radios = []
+    elements.forEach(this.handleElement)
+    this.flushRadioCache()
+    this.setState({ elements: this.inputs })
+  }
 
-    elements.forEach((element, index) => {
-      if (radios.length > 0 && (element.type !== 'radio' || element.name !== radios[0].name)) {
-        inputs.push(this.addInputRadio(radios, index))
-        radios = []
-      }
+  /**
+   * Takes and element and creates a sidebar input for it
+   * @param {Element} element
+   * @param {Int} index
+   */
+  handleElement (element, index) {
+    if (this.mustFlushRadio(element)) this.flushRadioCache()
 
-      switch (element.type) {
-        case 'text':
-          inputs.push(this.addInputText(element, index))
-          break
-        case 'textarea':
-          inputs.push(this.addInputTextarea(element, index))
-          break
-        case 'select-one':
-          inputs.push(this.addInputSelect(element, index))
-          break
-        case 'checkbox':
-          inputs.push(this.addInputCheckbox(element, index))
-          break
-        case 'radio':
-          radios.push(element)
-          break
-      }
-    })
+    switch (element.type) {
+      case 'text':
+        this.inputs.push(this.addInputText(element, index))
+        break
+      case 'textarea':
+        this.inputs.push(this.addInputTextarea(element, index))
+        break
+      case 'select-one':
+        this.inputs.push(this.addInputSelect(element, index))
+        break
+      case 'checkbox':
+        this.inputs.push(this.addInputCheckbox(element, index))
+        break
+      case 'radio':
+        this.radioCache.push(element)
+    }
+  }
 
-    this.setState({
-      elements: inputs
-    })
+  /**
+   * Checks if the element belongs to any previous radio inputs
+   * @param {Element} element
+   * @returns {Bool} True if element does not belong to any previous radio input
+   */
+  mustFlushRadio (element) {
+    if (!Array.isArray(this.radioCache) || this.radioCache.length < 1) return false
+    return (element.type !== 'radio' || element.name !== this.radioCache[0].name)
+  }
+
+  /**
+   * Add all previous radio inputs to the sidebar and clear radioCache
+   * @param {*} index
+   */
+  flushRadioCache (index) {
+    if (this.radioCache.length > 0) {
+      this.inputs.push(this.addInputRadio(this.radioCache, index))
+      this.radioCache = []
+    }
   }
 
   addInputCheckbox (element, index) {
