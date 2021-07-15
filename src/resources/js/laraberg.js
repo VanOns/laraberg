fe158515121e4063d33b2d03b5947215bfa42467
Maurice Wijnia <maurice@van-ons.nl>
2021-07-15 16:57:29 +0200

    Start work on v2 using the standalone block-editor

--- src/resources/js/laraberg.js
+++ src/resources/js/laraberg.js
@@ -1,22 +1,11 @@
-import 'babel-polyfill'
+import { initializeEditor } from '@mauricewijnia/block-editor'
 
-import './../scss/laraberg.scss'
-
-import './gutenberg/imports'
-import init from './gutenberg/init'
-import { getContent, setContent } from './lib/content'
-import { registerBlock, registerCategory } from './lib/custom-blocks'
-
-const Laraberg = {
-  init: init,
-  initGutenberg: init,
-  getContent: getContent,
-  setContent: setContent,
-  editor: null,
-  registerCategory: registerCategory,
-  registerBlock: registerBlock
+const init = (target, options = {}) => {
+  initializeEditor(target)
 }
 
-window.Laraberg = Laraberg
+const Laraberg = {
+  init
+}
 
 export default Laraberg
