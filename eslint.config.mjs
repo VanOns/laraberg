5f7c4741add5f83d0492e9e16871d4c24b632406
Patrick <patrick@van-ons.nl>
2024-08-16 14:22:19 +0200

    fix: Update node dependencies, add eslint, add nvmrc file

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  ...tseslint.configs.recommended,
+];
